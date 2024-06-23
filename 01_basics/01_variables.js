dd66dfb3605addf2d6d8bab3e5e73cc02679b255
Sagar-429 <naraesh.d@gmail.com>
2024-06-23 10:48:41 +0000

    variables datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId="144553"
+let accountEmail ="sagar.google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+//accountId="231" //
+
+accountEmail="sg@sg.com"
+accountPassword="45456"
+accountCity="Banglore"
+
+
+console.log(accountState)
+
+/*
+prefer not use var 
+because of issue in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
